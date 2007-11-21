ac1b0f784783e9b90a60874baf5e110ed7d9d0ae
Wybo Wiersma <wybo@logilogi.org>
2007-11-21 20:07:30 +0000

    Under the AGPL v3 now...

--- lib/diff/longest_common_sub_string/string.rb
+++ lib/diff/longest_common_sub_string/string.rb
@@ -3,14 +3,14 @@
 #
 # License:
 #   This file is part of the Diff-LongestCommonSubString library. Diff-
-#   LongestCommonSubString is free software. You can run/distribute/modify 
-#   Diff-LongestCommonSubString under the terms of the Affero General Public 
-#   License version 1 or any later version. The Affero GPL states that running 
-#   a modified version or a derivative work also requires you to make the 
-#   sourcecode of that work available to everyone that can interact with it. 
-#   We chose the Affero GPL to ensure that Diff-LongestCommonSubString remains 
-#   open and libre (doc/LICENSE.txt contains the full text of the legally 
-#   binding license).
+#   LongestCommonSubString is Free Software. You can run/distribute/modify 
+#   Diff-LongestCommonSubString under the terms of the GNU Affero General 
+#   Public License version 3. The Affero GPL states that running a modified
+#   version or a derivative work also requires you to make the sourcecode of
+#   that work available to everyone that can interact with it. We chose the
+#   Affero GPL to ensure that Diff-LongestCommonSubString remains open and
+#   libre (doc/LICENSE.txt contains the full text of the legally binding
+#   license).
 #++#
 
 require 'diff/longest_common_sub_string'
